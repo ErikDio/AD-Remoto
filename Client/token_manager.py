c7d309ab71dd7989c55695f54e2c45152f8101cd
Erik Dio <erik.theboy@yahoo.com.br>
2025-06-04 09:56:27 -0300

    Token logic updated
    Better logic for generating and validating token client-side, using Secrets

--- Client/token_manager.py
+++ Client/token_manager.py
@@ -1,25 +1,18 @@
-import time
+import secrets
 
 class TokenManager:
-    def __init__(self):
-        self._token = None
-        self._issued_at = None
-        self._expires_in = 3600  # seconds (1 hour)
+    token:str = None
 
-    def set_token(self, token):
-        self._token = token
-        self._issued_at = time.time()
+    @classmethod
+    def set_token(cls):
+        cls.token = secrets.token_hex(32)
+    
+    @classmethod
+    def get_token(cls):
+        if cls.token != None:
+            return cls.token
+        raise ValueError("Token inválido")
 
-    def get_token(self):
-        if self.is_token_valid():
-            return self._token
-        raise ValueError("Token is missing or expired")
-
-    def is_token_valid(self):
-        if self._token is None or self._issued_at is None:
-            return False
-        return (time.time() - self._issued_at) < self._expires_in
-
-    def clear_token(self):
-        self._token = None
-        self._issued_at = None
+    @classmethod
+    def clear_token(cls):
+        cls.token = None
