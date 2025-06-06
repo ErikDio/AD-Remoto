ff5838117a8fc97c8d7f411aca39cfb50c74f856
Erik Dio <erik.theboy@yahoo.com.br>
2025-06-06 13:03:33 -0300

    Update token_manager.py

--- Server/token_manager.py
+++ Server/token_manager.py
@@ -1,11 +1,16 @@
 class TokenManager:
     token:dict = {}
     @classmethod
-    def validate(cls, request_token):
+
+    def validate(cls, request_token): #Realiza validação do Token
         if request_token in cls.token.keys():
             return "placeholder"
         else:
             return "error"
+    
+    @classmethod
+    def add_token(cls, token, instance): #Adiciona um novo token e o atribui a uma instância
+        pass
 
     @classmethod
     def get_token(cls):
