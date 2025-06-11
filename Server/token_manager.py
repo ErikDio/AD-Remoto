8a665ba6c562946d3741e3613cae5d4e24dbd399
Erik Dio <erik.theboy@yahoo.com.br>
2025-06-11 13:45:59 -0300

    Update token_manager.py
    Defines and handles the expiration of tokens

--- Server/token_manager.py
+++ Server/token_manager.py
@@ -1,16 +1,22 @@
-from datetime import datetime
+import time
 
 class TokenManager:
     token:dict = {}
     @classmethod
     def validate(cls, request_token):
         try:
-            if cls.token[request_token]["expires"] < now:
+            if cls.token[request_token]["expires"] < time.time():
+                return "placeholder"
+            else:
+                cls.token[request_token]["expires"] = expiration_time()
                 return "placeholder"
         except KeyError:
             return "error"
 
     @classmethod
     def add_token(cls, request_token, object_id):
-        expiration = placeholder
+        expiration = expiration_time()
         cls.token[request_token] = {"expires":expiration, "object":object_id}
+
+def expiration_time() -> float:
+    return time.time() + (30*60)
