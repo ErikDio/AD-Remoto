83970a15051985e99ba2dec150c633cb4e1c3f70
Erik Dio <erik.theboy@yahoo.com.br>
2025-06-12 10:51:28 -0300

    Update token_manager.py
    Main logic for handling tokens implemented

--- Server/token_manager.py
+++ Server/token_manager.py
@@ -1,21 +1,30 @@
 import time
+from Shared.operations import *
 
 class TokenManager:
+    
     token:dict = {}
-    @classmethod
-    def validate(cls, request_token) -> str:
-        try:
-            if cls.token[request_token]["expires"] < time.time():
-                return "placeholder"
-            else:
-                cls.token[request_token]["expires"] = expiration_time()
-                return "placeholder"
-        except KeyError:
-            return "error"
 
     @classmethod
-    def add_token(cls, request_token, object_id):
-        expiration = expiration_time()
+    def validate(cls, request_token:str) -> str:
+        try:
+            if cls.token[request_token]["expires"] < time.time():
+                return ErrorList.EXPIRED_TOKEN
+            else:
+                return ReturnList.OPERATION_OK
+        except KeyError:
+            return ErrorList.INVALID_TOKEN
+        
+    @classmethod
+    def auth(cls, request_token:str) -> str:
+        if cls.validate(request_token) == ReturnList.OPERATION_OK:
+            return cls.token[request_token]["object"]
+        else:
+            return ReturnList.OPERATION_ERROR
+    
+    @classmethod
+    def add_token(cls, request_token:str, object_id:str):
+        expiration:float = expiration_time()
         cls.token[request_token] = {"expires":expiration, "object":object_id}
 
 def expiration_time() -> float:
